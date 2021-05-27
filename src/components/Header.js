2882e7866aed474e82bc30cab77981e276ed21ce
unknown <andres6459@gmail.com>
2021-05-26 19:37:49 -0700

    Create basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
+import './Header.css'
+import logo from '../assets/DanaMSLogo.png'
+
 const Header = () => {
   return(
-    <div>
-      This is React
-    </div>
+    <header>
+      <img src={logo} alt='Dana Middle School logo'/>
+      <h1>Dana Middle School Inventory</h1>
+      <button>Sign out</button>
+    </header>
   );
 }
 
