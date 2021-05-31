28fee25d2d3e64c56c4eae412a6b204a514761c6
unknown <andres6459@gmail.com>
2021-05-31 16:18:08 -0700

    Refactored AppBar into its own component from ResponsiveDrawer

---
+++ src/components/AppBarComp.js
@@ -0,0 +1,53 @@
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+const AppBarComp = ({ handleDrawerToggle }) => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Dana MS Inventory
+          </Typography>
+            <Button variant="contained" color="secondary">Sign out</Button>
+        </Toolbar>
+      </AppBar>
+    )
+}
+
+export default AppBarComp
--- src/components/ResponsiveDrawer.js
+++ src/components/ResponsiveDrawer.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import DevicesIcon from '@material-ui/icons/Devices';
 import QueuePlayNextIcon from '@material-ui/icons/QueuePlayNext';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import AddToQueueIcon from '@material-ui/icons/AddToQueue';
-import Button from '@material-ui/core/Button';
 import InventoryTable from './InventoryTable';
+import AppBarComp from './AppBarComp';
 
 const drawerWidth = 240;
 
@@ -33,22 +27,6 @@ const useStyles = makeStyles((theme) => ({
       width: drawerWidth,
       flexShrink: 0,
     },
-  },
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-
-  title: {
-    flexGrow: 1,
   },
   // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
@@ -95,23 +73,7 @@ function ResponsiveDrawer(props) {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap className={classes.title}>
-            Dana MS Inventory
-          </Typography>
-            <Button variant="contained" color="secondary">Sign out</Button>
-        </Toolbar>
-      </AppBar>
+      <AppBarComp handleDrawerToggle={handleDrawerToggle}/>
       <nav className={classes.drawer} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
@@ -150,13 +112,5 @@ function ResponsiveDrawer(props) {
     </div>
   );
 }
-
-ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
 
 export default ResponsiveDrawer;
