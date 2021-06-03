7c047e46a7b1c1cf6af576eda45cb9223786bb5f
Andres Rodriguez <andres6459@gmail.com>
2021-06-03 12:55:01 -0700

    Added search bar to table using Grid.

--- src/components/InventoryTableSearch.js
+++ src/components/InventoryTableSearch.js
@@ -1,4 +1,4 @@
-import { InputLabel, MenuItem, Select, TextField } from "@material-ui/core"
+import { InputLabel, MenuItem, Select, TextField, Grid } from "@material-ui/core"
 import FormControl from '@material-ui/core/FormControl';
 import React from 'react'
 
@@ -9,25 +9,29 @@ const InventoryTableSearch = () => {
     }
 
     return(
-        <div>
             <FormControl>
-                <InputLabel id='search-label'>Table Label</InputLabel>
-                <Select
-                    labelId='search-label'
-                    id='search-label'
-                    value={tableLabel}
-                    onChange={handleChange}
-                    >
-                    <MenuItem value='deviceType'>Device Type</MenuItem>
-                    <MenuItem value='manufacture'>Manufacture</MenuItem>
-                    <MenuItem value='model'>Model</MenuItem>
-                    <MenuItem value='assetTag'>Asset Tag</MenuItem>
-                    <MenuItem value='checkedOutTo'>Checked out to</MenuItem>
-                    <MenuItem value='room'>Room</MenuItem>
-                </Select>
-                <TextField id="standard-search" label="Search field" type="search" />
+                <Grid container direction='row' justify='space-between' spacing={1}>
+                    <Grid item xs={6}>              
+                        <InputLabel id='search-label'>Table Label</InputLabel>
+                        <Select
+                            labelId='search-label'
+                            id='search-label'
+                            value={tableLabel}
+                            onChange={handleChange}
+                            >
+                            <MenuItem value='deviceType'>Device Type</MenuItem>
+                            <MenuItem value='manufacture'>Manufacture</MenuItem>
+                            <MenuItem value='model'>Model</MenuItem>
+                            <MenuItem value='assetTag'>Asset Tag</MenuItem>
+                            <MenuItem value='checkedOutTo'>Checked out to</MenuItem>
+                            <MenuItem value='room'>Room</MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField id="standard-search" label="Search field" type="search" fullWidth/>
+                    </Grid>
+                </Grid>
             </FormControl>
-        </div>
     )
 }
 
